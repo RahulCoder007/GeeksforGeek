7af5dd107a54607869065dae6aad6728b9fcb409
RahulCoder007 <rahul.ver007@live.com>
2022-05-18 21:55:35 +0530

    Add files via upload

---
+++ Convert String to LowerCase.cpp
@@ -0,0 +1,33 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    string toLower(string S) {
+         //string x;
+      for(int i=0;i<S.length();i++)
+     S[i]=tolower(S[i]);
+      return S;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) 
+    {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.toLower(S) << endl;
+    }
+    return 0;
+}
+  // } Driver Code Ends
